e312903d59ca92e3c29e929fd021b86752ccb3cf
Алексей Кузьмин <akuzmin340@gmail.com>
2025-05-06 23:39:49 +0200

    fix bug

--- internal/server/rest/util.go
+++ internal/server/rest/util.go
@@ -13,13 +13,13 @@ const errEncodingJson = "json encode error"
 func (h *Handler) writeAndLogErr(ctx context.Context, w http.ResponseWriter, err error, status int) {
 	w.Header().Set("Content-Type", "application/json")
 
+	w.WriteHeader(status)
+
 	if err := json.NewEncoder(w).Encode(models.ErrorResponse{Error: err.Error()}); err != nil {
 		h.l.LogAttrs(ctx, slog.LevelError, errEncodingJson, slog.String("err", err.Error()))
 	}
 
 	h.l.LogAttrs(ctx, slog.LevelError, "error handling request", slog.String("err", err.Error()))
-
-	w.WriteHeader(status)
 }
 
 func (h *Handler) writeJson(ctx context.Context, w http.ResponseWriter, v any, status int) {
